443176a08f625d858088db038414e66bae02f282
Vasily (DevExpress Support) <vasiliy.stepanov@devexpress.com>
2021-04-06 21:23:10 +0300

    Remove Silverlight attribute

--- CS/Reporting/ReportService1.svc.cs
+++ CS/Reporting/ReportService1.svc.cs
@@ -4,7 +4,6 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Activation;
 using DevExpress.Data.Utils.ServiceModel;
-using DevExpress.Xpf.Printing.Service;
 using DevExpress.XtraReports.Service;
 using DevExpress.XtraReports.UI;
 using ReportService;
@@ -12,7 +11,6 @@ using ReportService.Reports;
 
 namespace Reporting {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ReportService1" in code, svc and config file together.
-    [SilverlightFaultBehavior]
     public class ReportService1 : DevExpress.XtraReports.Service.ReportService {
 
     }
