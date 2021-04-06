dd846e17e6a340574bf7855a8f2ef0b4cec1d78e
Vasily (DevExpress Support) <vasiliy.stepanov@devexpress.com>
2021-04-06 21:53:51 +0300

    Migrate to RemoteDocumentSource

--- CS/MinimalisticReportPreviewDemo/MainWindow.xaml.cs
+++ CS/MinimalisticReportPreviewDemo/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using DevExpress.ReportServer.Printing;
 using DevExpress.Xpf.Core;
 using DevExpress.Xpf.Printing;
 // ...
@@ -14,12 +15,13 @@ namespace MinimalisticReportPreviewDemo {
         }
 
         private void ShowPreview(object sender, RoutedEventArgs e) {
-            ReportServicePreviewModel model = new ReportServicePreviewModel();
-            model.ServiceUri = @"http://localhost:50786/ReportService1.svc";
-            model.ReportName = "ReportService.Reports.Report, Reporting";
+            RemoteDocumentSource remoteSource = new RemoteDocumentSource();
+            remoteSource.ServiceUri = @"http://localhost:50786/ReportService1.svc";
+            remoteSource.ReportName = "ReportService.Reports.Report, Reporting";
 
-            DocumentPreviewWindow window = new DocumentPreviewWindow() { Model = model };
-            model.CreateDocument();
+            DocumentPreviewWindow window = new DocumentPreviewWindow();
+            window.PreviewControl.DocumentSource = remoteSource;
+            remoteSource.CreateDocument();
 
             window.ShowDialog();
         }
